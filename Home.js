7ced34981492a155e5ea8f55a86e510f74290e36
tyusliu <ttl002@ucsd.edu>
2023-03-23 03:34:17 -0700

    Just made a home screen and pi screen with routes

---
+++ Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {
+	StyleSheet,
+	Button,
+	View,
+	SafeAreaView,
+	Text,
+	Alert,
+} from 'react-native';
+
+export default function Home({ navigation }) {
+	return (
+		<Button
+			title='Go to Pi Game'
+			onPress={() => navigation.navigate('Pi')}
+		/>
+	);
+}
